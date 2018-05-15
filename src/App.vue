d40a350865c8c9dc13168e5547116bd4ed6dc7b0
dcc <cumelmell@foxmail.com>
2018-05-15 23:04:34 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- 头部组件 -->
+    <v-header></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 引入header头部组件
+import header from '@/components/header/header'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  },
+  components: {
+    // 引入头部组件
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- 头部容器 -->
+  <div class="header">
+    <!-- 头部主体 -->
+    <div class="content-wrapper">
+      <!-- 头像 -->
+      <div class="avatar">
+        <img src="" alt="" width="64" height="64">
+      </div>
+      <!-- 右侧店家信息 -->
+      <div class="content">
+        <!-- 店家名字 -->
+        <div class="title">
+          商家名字
+        </div>
+        <!-- 送货描述 -->
+        <div class="description">
+          XXX分钟送达
+        </div>
+        <!-- 点击优惠满减 -->
+        <div class="support">
+          在线支付XXXX
+        </div>
+        <!-- 优惠类型 -->
+        <div class="supports-count">
+          几个支持
+        </div>
+      </div>
+    </div>
+    <!-- 商家滚动介绍 -->
+    <div class="bulletin-wrapper">
+      商家滚动介绍
+    </div>
+    <!-- 头部背景 -->
+    <div class="background">
+      头部背景
+      <img src="" alt="">
+    </div>
+    <!-- 商家星级弹出层 -->
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      detailShow: false
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
